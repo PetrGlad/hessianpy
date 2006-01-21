6b69d48f7e9fa2e346b24321f892f0316b7f6bfd
batyi <batyi@c54368c1-2727-4063-95f6-e541575462ff>
2006-01-21 10:09:39 +0000

    version number corrected

--- hessianpy/hessian/client.py
+++ hessianpy/hessian/client.py
@@ -1,12 +1,11 @@
 #
 # Hessian protocol implementation
-#
-# Protocol specification can be found here
-# http://www.caucho.com/resin-3.0/protocols/hessian-1.0-spec.xtp
-#
 # This file contains client proxy code.
 #
-# Copyright 2005 Petr Gladkikh (batyi at mail ru)
+# Protocol specification can be found here:
+# http://www.caucho.com/resin-3.0/protocols/hessian-1.0-spec.xtp
+#
+# Copyright 2005, 2006 Petr Gladkikh (batyi at sourceforge net)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -25,7 +24,7 @@ import urllib
 import httplib
 from StringIO import StringIO
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 
 class Method:
     "Encapsulates the method to be called"
@@ -44,11 +43,11 @@ class HttpProxy:
     
     def __init__(self, url):
         self.url = url
-        transport, uri = urllib.splittype(url)	
+        transport, uri = urllib.splittype(url)    
         if transport != "http":
             raise IOError("Unsupported transport protocol '" + transport + "'")
         self.host, self.uri = urllib.splithost(uri)
-	
+    
     def __invoke(self, method, params):        
         s = StringIO()
         hessian.writeObject(hessian.WriteContext(s), \
@@ -68,11 +67,12 @@ class HttpProxy:
             raise Exception("HTTP Error %d: %s" % (response.status, response.reason))
 
         inStream = StringIO(response.read())
+        # print "\nGot buffer:[" + inStream.buf + "]" # debug
         ctx = hessian.ParseContext(inStream)
         (headers, status, value) = hessian.Reply().read(ctx, ctx.read(1))        
-        # print "RESULT:", headers, status, value        
+        # print "Call result:", headers, status, value # debug
         if not status:
-            # value is a Hessian error description 
+            # value is a Hessian error description
             raise Exception(value) 
         return value
 
